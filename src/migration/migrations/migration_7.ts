82a373ceb430895e06d834bc788060caee42828d
johanfrost <johan@frostdigital.se>
2023-12-17 14:56:54 +0100

    Tracking right migration

--- src/migration/migrations/migration_7.ts
+++ src/migration/migrations/migration_7.ts
@@ -5,5 +5,5 @@ export async function Migration7(){
     //Add index to assets
     (await collections.asset.collection()).createIndex({ assetId : 1}, { name : "assetId"})
 
-    await collections.migration.create({ version : 5, date : new Date()})
+    await collections.migration.create({ version : 7, date : new Date()})
 }
